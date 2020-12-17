648f08a859b3f30723f6dc1e184a324fb7a7f159
william <william@thedigitalacademy.co.za>
2020-12-17 15:14:01 +0200

    comments added

--- src/app/Services/ShortestPath.service.ts
+++ src/app/Services/ShortestPath.service.ts
@@ -1,37 +1,71 @@
-import { Injectable } from '@angular/core';
-import { GRAPH, PLANET_NAMES } from '../Graph/Graph';
+import { Injectable } from "@angular/core";
+import { GRAPH, PLANET_NAMES } from "../Graph/Graph";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ShortestPath {
-
-  INFINITY:number = 9999;
-    myNodes :string[]=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-"A'","B'","C'","D'","E'","F'","G'","H'","I'","J'","K'","L'"
+  private INFINITY: number = 9999;
+  private myNodes: string[] = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "A'",
+    "B'",
+    "C'",
+    "D'",
+    "E'",
+    "F'",
+    "G'",
+    "H'",
+    "I'",
+    "J'",
+    "K'",
+    "L'",
   ];
 
-
-  private planetsNames:any[] = PLANET_NAMES;
+  private planetsNames: any[] = PLANET_NAMES;
   private nodeMax = 38; //max node number
-  private shortestPaths:any[]=[];
-  private tempObject:any={
-    nodeid:'',
-    nodePath:[],
-    distance:0
+  private shortestPaths: any[] = [];
+  private tempObject: any = {
+    nodeid: "",
+    nodePath: [],
+    distance: 0,
   };
- private paths:any[];
+  private paths: any[];
 
-constructor() {}
+  constructor() {}
 
-/**
- *
- * @param G represents a the graph
- * @param n number of nodes
- * @param startnode is a variable that holds the starting node
- */
-  dijkstra(G: number[][], n: number, startnode: number) :void{
-
+  /**
+   *
+   * @param G represents a the graph
+   * @param n number of nodes
+   * @param startnode is a variable that holds the starting node
+   */
+  dijkstra(G: number[][], n: number, startnode: number): void {
     let cost: number[][] = new Array(this.nodeMax)
       .fill(0)
       .map(() => new Array(this.nodeMax).fill(0));
@@ -82,75 +116,62 @@ constructor() {}
       count++;
     }
 
-
     for (i = 0; i < n; i++) {
       if (i != startnode) {
         //  console.log("Distance of node", i, "=", distance[i]);
 
         //  console.log("Path=", i);
-        this.tempObject['nodeid'] = i;
-        this.tempObject['distance'] = distance[i];
+        this.tempObject["nodeid"] = i;
+        this.tempObject["distance"] = distance[i];
         j = i;
-        this.tempObject['nodePath'].push(j)
+        this.tempObject["nodePath"].push(j);
         do {
           j = pred[j];
           //console.log("<-", j);
-          this.tempObject['nodePath'].push(j);
-
+          this.tempObject["nodePath"].push(j);
         } while (j != startnode);
 
         this.shortestPaths.push(this.tempObject);
-        this.tempObject={
-          nodeid:'',
-          nodePath:[],
-          distance:0
-        }
-
+        this.tempObject = {
+          nodeid: "",
+          nodePath: [],
+          distance: 0,
+        };
       }
     }
-
-
-
   }
 
-   /**
-     * getShortestPaths()
-     * this method returns an array of all the paths
-    */
+  /**
+   * getShortestPaths()
+   * this method returns an array of all the paths
+   */
 
-   public getShortestPaths():any[] {
+  public getShortestPaths(): any[] {
     // console.log(this.planetsNames);
     return this.planetsNames;
   }
 
-  setCharecterPath(){
+  setCharecterPath() {
     this.paths = [];
-    this.shortestPaths
-    .forEach((temp,index)=>{
-     let nodeCharecter = this.myNodes[temp.nodeid];
-     let distance = temp.distance;
-     let tempPath:any[] =[];
+    this.shortestPaths.forEach((temp, index) => {
+      let nodeCharecter = this.myNodes[temp.nodeid];
+      let distance = temp.distance;
+      let tempPath: any[] = [];
 
-       for(let i =0 ; i < temp.nodePath.length ; i++){
-         tempPath.push(this.myNodes[temp.nodePath[i]]);
-       }
+      for (let i = 0; i < temp.nodePath.length; i++) {
+        tempPath.push(this.myNodes[temp.nodePath[i]]);
+      }
 
-        this.planetsNames[index]['path'] = tempPath.reverse();
-        this.planetsNames[index]['distance'] = distance;
+      this.planetsNames[index]["path"] = tempPath.reverse();
+      this.planetsNames[index]["distance"] = distance;
 
-        this.paths.push({
-          node:nodeCharecter,
-          path:tempPath.reverse(),
-          distance:distance
-        });
+      this.paths.push({
+        node: nodeCharecter,
+        path: tempPath.reverse(),
+        distance: distance,
+      });
 
-       tempPath = [];
-
-    })
-
+      tempPath = [];
+    });
   }
-
-
-
-
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,61 +1,66 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { GRAPH, PLANET_NAMES } from './Graph/Graph';
-import { ShortestPath } from './Services/ShortestPath.service';
-
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
+import { GRAPH, PLANET_NAMES } from "./Graph/Graph";
+import { ShortestPath } from "./Services/ShortestPath.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'mdb-angular-free';
-  titleColumn: any = ['name','symbol','distnace','path'];
-  planetData:any[] =[];
+  title = "mdb-angular-free";
+  titleColumn: any = ["name", "symbol", "distnace", "path"];
+  planetData: any[] = [];
 
-  selectedPlanet:any = " ";
+  selectedPlanet: any = " ";
 
-  headElements = ['ID', 'First', 'Last', 'Handle'];
-  imagePath ='../assets/img/Earth.png';
-  planetsNames:any[] =PLANET_NAMES;
-  graph:number [][] =GRAPH;
+  headElements = ["ID", "First", "Last", "Handle"];
+  imagePath = "../assets/img/Earth.png";
+  planetsNames: any[] = PLANET_NAMES;
+  graph: number[][] = GRAPH;
 
-  constructor(
-    public shortestPath:ShortestPath,
-    private http:HttpClient
-    ){
-    this.shortestPath.dijkstra(GRAPH,GRAPH.length,0);
+  constructor(public shortestPath: ShortestPath, private http: HttpClient) {
+    this.shortestPath.dijkstra(GRAPH, GRAPH.length, 0);
     this.shortestPath.setCharecterPath();
     this.shortestPath.getShortestPaths();
   }
-  onChange(newValue) {
-    console.log(newValue);
+  onChange(planet) {
+    console.log(planet);
     this.planetData = [];
-    this.selectedPlanet = newValue.name;
+    this.selectedPlanet = planet.name;
 
-    this.getFolder(newValue.name)
+    this.getFolder(planet.name);
+    this.populatePlanetData(planet);
 
+    if (planet.path[0] != "A") this.planetData.push(planet.path.reverse());
+    else this.planetData.push(planet.path);
+  }
+  /**
+   *
+   * @param planet holds a selected planet data
+   * the mothod populate the data into planet data array so it can show on the table
+   */
+  populatePlanetData(planet:any) {
+    this.planetData.push(planet.name);
+    this.planetData.push(planet.nodeSymbol);
+    this.planetData.push(planet.distance);
+  }
 
-    this.planetData.push(newValue.name);
-    this.planetData.push(newValue.nodeSymbol);
-    this.planetData.push(newValue.distance);
-    
-      if(newValue.path[0] != 'A')
-        this.planetData.push(newValue.path.reverse());
-      else
-        this.planetData.push(newValue.path);
-
-
-
-
-}
-
-getFolder(name: string) {
-
-  console.log(encodeURI(`../assets/img/${name}.png`))
-    this.http.get(`../assets/img/${name}.png`,{ responseType: 'text' })
-    .subscribe(_=>this.imagePath=encodeURI(`../assets/img/${name}.png`),
-    error=>this.imagePath=encodeURI('../assets/img/image-placeholder-icon-6.jpg'))
-}
+  /**
+   *
+   * @param name contains that can be passed to the uri  
+   */
+  getFolder(name: string) {
+    console.log(encodeURI(`../assets/img/${name}.png`));
+    this.http
+      .get(`../assets/img/${name}.png`, { responseType: "text" })
+      .subscribe(
+        (_) => (this.imagePath = encodeURI(`../assets/img/${name}.png`)),
+        (error) =>
+          (this.imagePath = encodeURI(
+            "../assets/img/image-placeholder-icon-6.jpg"
+          ))
+      );
+  }
 }
